4dfdc77f79450e881492c0b57d2473399231050f
Raimond Spekking <raymond@users.mediawiki.org>
2011-12-21 20:44:11 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- FeaturedFeeds.i18n.php
+++ FeaturedFeeds.i18n.php
@@ -51,41 +51,65 @@ $messages['en'] = array(
 	'ffeed-motd-entry' => '{{SITENAME}} quote of the day for {{LOCALMONTHNAME}} {{LOCALDAY}}',
 );
 
-/** 
- * Message documentation
+/** Message documentation (Message documentation)
  * @author Max Semenik
  */
 $messages['qqq'] = array(
 	'ffeed-desc' => '{{desc}}',
-
-	# Featured Article
 	'ffeed-fa-title' => 'Title of the Featured Articles [[w:web feed|syndication feed]]',
 	'ffeed-fa-desc' => 'Description of the Featured Articles [[w:web feed|syndication feed]]',
-	'ffeed-fa-entry' => 'Title of day\'s entry in the Featured Articles [[w:web feed|syndication feed]]',
-
-	# On this day...
+	'ffeed-fa-entry' => "Title of day's entry in the Featured Articles [[w:web feed|syndication feed]]",
 	'ffeed-onthisday-title' => 'Title of the "On this day..." [[w:web feed|syndication feed]]',
 	'ffeed-onthisday-desc' => 'Description of the "On this day..." [[w:web feed|syndication feed]]',
 	'ffeed-onthisday-entry' => 'Title of day\'s entry in the "On this day..." [[w:web feed|syndication feeds]',
-
-	# Did You Know?
 	'ffeed-dyk-title' => 'Title of the "Did you know?" [[w:web feed|syndication feed]]',
 	'ffeed-dyk-desc' => 'Description of the "Did you know?" [[w:web feed|syndication feed]]',
 	'ffeed-dyk-entry' => 'Title of day\'s entry in the "Did you know?" [[w:web feed|syndication feeds]',
-
-	// Media Of The Day
 	'ffeed-motd-title' => 'Title of the Media of the Day [[w:web feed|syndication feed]]',
 	'ffeed-motd-desc' => 'Description of the Media of the Day [[w:web feed|syndication feed]]',
-	'ffeed-motd-entry' => 'Title of day\'s entry in the Media of the Day [[w:web feed|syndication feed]]',
-
-	# Picture Of The Day
+	'ffeed-motd-entry' => "Title of day's entry in the Media of the Day [[w:web feed|syndication feed]]",
 	'ffeed-potd-title' => 'Title of the Picture Of The Day [[w:web feed|syndication feed]]',
 	'ffeed-potd-desc' => 'Description of the Picture Of The Day [[w:web feed|syndication feed]]',
-	'ffeed-potd-entry' => 'Title of day\'s entry in the Media of the Day [[w:web feed|syndication feed]]',	
+	'ffeed-potd-entry' => "Title of day's entry in the Media of the Day [[w:web feed|syndication feed]]",
+);
 
-	# Quote Of The Day
-	'ffeed-qotd-title' => 'Title of the Quote Of The Day [[w:web feed|syndication feed]]',
-	'ffeed-qotd-desc' => 'Description of the Quote Of The Day [[w:web feed|syndication feed]]',
-	'ffeed-qotd-entry' => 'Title of day\'s entry in the Quote of the Day [[w:web feed|syndication feed]]',	
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'ffeed-desc' => 'Dodawa syndikaciske kanale wuběrnego wikiwopśimjeśa.',
+	'ffeed-fa-title' => '{{SITENAME}} - kanal wuběrnych nastawkow',
+	'ffeed-fa-desc' => 'Nejlěpše nastawki, kótarež {{SITENAME}} póbitujo',
+	'ffeed-fa-entry' => '{{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}  {{SITENAME}} - wuběrny nastawk',
+	'ffeed-onthisday-title' => '{{SITENAME}} - kanal "Toś ten źeń..."',
+	'ffeed-onthisday-desc' => 'Historiske tšojenja na toś ten źeń',
+	'ffeed-onthisday-entry' => 'Toś ten źeń: {{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}',
+	'ffeed-dyk-title' => '{{SITENAME}} - kanal "Sćo wěželi, až...?"',
+	'ffeed-dyk-desc' => 'Nejnowše wopśimjeśe z {{GRAMMAR:genitiw|{{SITENAME}}}}',
+	'ffeed-dyk-entry' => 'Sćo wěźeli, až...?: {{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}',
+	'ffeed-motd-title' => '{{SITENAME}} - kanal citata dnja',
+	'ffeed-motd-desc' => 'Někotare z nejlěpšych citatow na {{GRAMMAR:lokatiw|{{SITENAME}}}}',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'ffeed-desc' => 'Přidawa syndikaciske kanale wuběrneho wikijoweho wobsaha.',
+	'ffeed-fa-title' => '{{SITENAME}} - kanal wuběrnych nastawkow',
+	'ffeed-fa-desc' => 'Najlěpše nastawki, kotrež {{SITENAME}} poskića',
+	'ffeed-fa-entry' => '{{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}  {{SITENAME}} wuběrny nastawk',
+	'ffeed-onthisday-title' => '{{SITENAME}} - kanal "Tutón dźeń..."',
+	'ffeed-onthisday-desc' => 'Historiske podawki na tutón dźeń',
+	'ffeed-onthisday-entry' => 'Tutón dźeń: {{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}',
+	'ffeed-dyk-title' => '{{SITENAME}} - kanal "Wěš ty, zo...?"',
+	'ffeed-dyk-desc' => 'Najnowši wobsah z {{GRAMMAR:genitiw|{{SITENAME}}}}',
+	'ffeed-dyk-entry' => 'Wěš ty, zo...?: {{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}',
+	'ffeed-motd-title' => '{{SITENAME}} - kanal citata dnja',
+	'ffeed-motd-desc' => 'Někotre z najlěpšich citatow na {{GRAMMAR:lokatiw|{{SITENAME}}}}',
+	'ffeed-motd-entry' => '{{SITENAME}} - citat dnja za {{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}',
+	'ffeed-potd-title' => '{{SITENAME}} - kanal wobraz dnja',
+	'ffeed-potd-desc' => 'Někotre z najlěpšich wobrazow na {{GRAMMAR:lokatiw|{{SITENAME}}}}',
+	'ffeed-potd-entry' => '{{SITENAME}} - wobraz dnja za {{LOCALDAY}}. {{LOCALMONTHNAMEGEN}}',
 );
 
