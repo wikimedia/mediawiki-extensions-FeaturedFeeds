c83cbbda179e03573061ebcc6f4fedeba0a27dfa
Kunal Mehta <legoktm@gmail.com>
2015-03-04 00:05:01 -0800

    Add extension.json, empty PHP entry point
    Bug: T87914
    Change-Id: Icfa224569327f0ad6ae952ff91d1d084aaba240d

--- FeaturedFeeds.php
+++ FeaturedFeeds.php
@@ -1,52 +1,14 @@
 <?php
-/*
- * Featured Feed extension by Max Semenik.
- * License: WTFPL 2.0
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not a valid entry point' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'FeaturedFeeds' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['FeaturedFeeds'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['FeaturedFeedsAliases'] = __DIR__ . '/FeaturedFeeds.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for FeaturedFeeds extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the FeaturedFeeds extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'FeaturedFeeds',
-	'author' => array( 'Max Semenik' ),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:FeaturedFeeds',
-	'descriptionmsg' => 'ffeed-desc',
-);
-
-$wgAutoloadClasses['ApiFeaturedFeeds'] = __DIR__ . '/ApiFeaturedFeeds.php';
-$wgAutoloadClasses['FeaturedFeeds'] = __DIR__ . '/FeaturedFeeds.body.php';
-$wgAutoloadClasses['FeaturedFeedChannel'] = __DIR__ . '/FeaturedFeeds.body.php';
-$wgAutoloadClasses['FeaturedFeedItem'] = __DIR__ . '/FeaturedFeeds.body.php';
-$wgAutoloadClasses['SpecialFeedItem'] = __DIR__ . '/SpecialFeedItem.php';
-
-$wgMessagesDirs['FeaturedFeeds'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['FeaturedFeeds'] =  __DIR__ . '/FeaturedFeeds.i18n.php';
-$wgExtensionMessagesFiles['FeaturedFeedsAliases'] =  __DIR__ . '/FeaturedFeeds.alias.php';
-
-$wgSpecialPages['FeedItem'] = 'SpecialFeedItem';
-
-$wgAPIModules['featuredfeed'] = 'ApiFeaturedFeeds';
-
-$wgHooks['ArticleSaveComplete'][] = 'FeaturedFeeds::articleSaveComplete';
-$wgHooks['BeforePageDisplay'][] = 'FeaturedFeeds::beforePageDisplay';
-$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'FeaturedFeeds::skinTemplateOutputPageBeforeExec';
-
-/**
- * Configuration settings
- */
-
-$wgFeaturedFeedsDefaults = array(
-	'limit' => 10,
-	'frequency' => 'daily',
-	'inUserLanguage' => false,
-);
-
-$wgFeaturedFeeds = array();
-
-/**
- * Whether links to feeds should appear in sidebar
- */
-$wgDisplayFeedsInSidebar = true;
